158f1bab601393727caa235267831c6d8c010eb0
“Ryben” <sebastianjimenezroman@hotmail.com>
2022-06-04 17:12:44 +0200

    just some backgrounds

--- el-tiempo-sebastian/src/main.js
+++ el-tiempo-sebastian/src/main.js
@@ -4,6 +4,13 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// library.add(faUserSecret)
+// Vue.component('font-awesome-icon', FontAwesomeIcon)
+// Vue.config.productionTip = false
+
 // import Vue from 'vue'
 // import vSelect from 'vue-select'
 // import 'vue-select/dist/vue-select.css';
